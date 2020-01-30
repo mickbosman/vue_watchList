479f89144ad62328624e54fc24f78dce53afafb9
mickbosman <42744703+mickbosman@users.noreply.github.com>
2020-01-30 12:14:15 +0100

    30 min

---
+++ src/components/FilmItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="film-item" >
+    <p>{{film.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilmItem",
+  props: ["film"]
+}
+
+
+</script>
+
+<style scoped>
+  .film-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
---
+++ src/components/Films.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="film.id" v-for="film in films">
+      <FilmItem v-bind:film="film" />
+  </div>
+</div>
+</template>
+
+<script>
+import FilmItem from './FilmItem.vue';
+
+  export default {
+    name: "Films",
+    components: {
+      FilmItem
+    },
+    props: ["films"]
+  }
+</script>
+
+<style scoped>
+
+</style>
